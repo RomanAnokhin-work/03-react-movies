3f9ec29e54c72f98823a7251e26ed0e8db92ee6a
Roman <roman.anokhin.work@gmail.com>
2025-11-07 01:28:28 +0200

    Add components and content

---
+++ src/components/App/App.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import css from "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import getMovies from "../../services/movieService";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+import type { Movie } from "../../types/movie";
+import toast, { Toaster } from "react-hot-toast";
+
+function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleQuery = async (query: string) => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      const response = await getMovies(query);
+      if (!movies) {
+        toast.error("No movies found for your request.");
+      }
+      setMovies(response);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+ 
+  const openModal = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedMovie(null);
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleQuery} />
+      {movies.length > 0 ? (
+        <MovieGrid onSelect={openModal} movies={movies} />
+      ) : null}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {isModalOpen && selectedMovie && <MovieModal onClose={closeModal} movie={selectedMovie} />}
+      <Toaster position="top-center" reverseOrder={false} />
+    </div>
+  );
+}
+
+export default App;
